2e0be5837ab1a40ab9fc78f756873eb6a4af5a64
mBadar-dev <m.badar.dev@gmail.com>
2025-01-09 14:42:36 +0500

    data base connect

--- data base/dataBase.js
+++ data base/dataBase.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () =>{
     try {
         const connect = await mongoose.connect(process.env.URI)
-
+        console.log("Moongose connection is successfully done!");
     } catch (error) {
         console.log("Cannot connect to the Moongose!")
         console.log(error)
